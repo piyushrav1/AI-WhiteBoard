e45d7edf2c1d0785ab3abb0f94fa646b4e12fa96
Piyush Ravi <piyushravi.tiu@gmail.com>
2025-05-15 00:32:11 +0530

    basic Node.js + Socket.IO backend

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",  // replace with your client URL in production
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
